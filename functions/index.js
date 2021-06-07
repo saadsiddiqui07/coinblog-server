389ec3fb46aa528f0515c91ab7afeaf8f6ce3c98
Saad Siddiqui <saadsiddiqui927@gmail.com>
2021-06-07 19:06:04 +0530

    Like notification

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 // IMPORTS
 const functions = require("firebase-functions");
 const app = require("express")();
-const { admin } = require("./utils/admin");
+const { admin, db } = require("./utils/admin");
 const cors = require("cors");
 app.use(cors());
 
@@ -38,7 +38,7 @@ app.post("/post/:postId/comment", FirebaseAuth, commentOnPost);
 // like a single post
 app.get("/post/:postId/like", FirebaseAuth, likePost);
 // unlike a post
-app.get("/post/:postId/unlike,", FirebaseAuth, unlikePost);
+app.get("/post/:postId/unlike", FirebaseAuth, unlikePost);
 
 // USER ROUTES
 // create a new user
@@ -53,3 +53,32 @@ app.post("/user", FirebaseAuth, addUserDetails);
 app.get("/user", FirebaseAuth, getAuthenticatedUser);
 
 exports.api = functions.region("asia-south1").https.onRequest(app);
+
+// notifications
+exports.createNotificationOnLike = functions
+  .region("asia-south1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            postId: doc.postId,
+          });
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
+
